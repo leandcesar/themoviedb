4479e7d121f1a2207e3b37f0ca09366ec41304fc
Leandro César <ccleandroc@gmail.com>
2023-03-11 15:46:35 -0300

    feat: add support for `EpisodeGroups`

---
+++ themoviedb/routes/episode_groups.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from themoviedb import schemas, utils
+from themoviedb.routes.base import Base
+
+
+class EpisodeGroup(Base):
+    def __init__(self, episode_group_id: int, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.episode_group_id = episode_group_id
+
+    async def details(self) -> schemas.EpisodeGroup:
+        """Get the details of a TV episode group.
+
+        See more: https://developers.themoviedb.org/3/tv-episode-groups/get-tv-episode-group-details
+        """
+        data = await self.request(f"tv/episode_group/{self.episode_group_id}")
+        return utils.as_dataclass(schemas.EpisodeGroup, data)
--- themoviedb/schemas/episode_groups.py
+++ themoviedb/schemas/episode_groups.py
@@ -2,8 +2,17 @@ from dataclasses import dataclass
 from typing import List, Optional
 
 from themoviedb.schemas._enums import EpisodeGroupType
-from themoviedb.schemas._partial import PartialNetwork
+from themoviedb.schemas._partial import PartialEpisode, PartialNetwork
 from themoviedb.schemas._result import ResultWithID
+
+
+@dataclass
+class Group:
+    id: Optional[str] = None
+    name: Optional[str] = None
+    order: Optional[int] = None
+    episodes: Optional[List[PartialEpisode]] = None
+    locked: Optional[bool] = None
 
 
 @dataclass
@@ -12,6 +21,7 @@ class EpisodeGroup:
     description: Optional[str] = None
     episode_count: Optional[int] = None
     group_count: Optional[int] = None
+    groups: Optional[List[Group]] = None
     name: Optional[str] = None
     network: Optional[PartialNetwork] = None
     type: Optional[EpisodeGroupType] = None
